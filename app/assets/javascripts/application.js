37e56b210dddc670b79df74ad1be38a77d7e8004
lwangenheim <lwangenheim@gmail.com>
2014-04-21 20:43:11 -0400

    trying this

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require angular
+//= require underscore
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require underscore
-//= require angular
